ed065eccffcb3cce8525a523dd63a32c0be95a62
preki <preki@192.168.0.197>
2016-08-03 19:27:25 -0700

    refactor packages

--- src/Quadrilateral.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Quadrilateral extends Polygon {
-
-	public Quadrilateral() {
-		super(4);
-	}
-
-}
--- src/RendererInterface.java
+++
@@ -1,9 +0,0 @@
-
-
-/*
- 	*Use whenever classs/object HAS-A single renderer 
-*/
-public interface RendererInterface {
-	public void render();
-	public void dispose();
-}
