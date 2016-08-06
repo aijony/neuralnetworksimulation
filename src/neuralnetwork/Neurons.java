dae24848d798e500c63cef21b50016007c6ed1e0
aijony <aijony@users.noreply.github.com>
2016-08-05 20:31:03 -0700

    Rename neuron to neurons

--- src/neuralnetwork/Neurons.java
+++ src/neuralnetwork/Neurons.java
@@ -1,7 +1,7 @@
 package neuralnetwork;
 import java.util.*;
  
-public class Neuron {   
+public class Neurons {   
     static int counter = 0;
     final public int id;  // auto increment, starts at 0
     Connection biasConnection;
@@ -11,7 +11,7 @@ public class Neuron {
     ArrayList<Connection> Inconnections = new ArrayList<Connection>();
     HashMap<Integer,Connection> connectionLookup = new HashMap<Integer,Connection>();
      
-    public Neuron(){        
+    public Neurons(){        
         id = counter;
         counter++;
     }
@@ -22,7 +22,7 @@ public class Neuron {
     public void calculateOutput(){
         double s = 0;
         for(Connection con : Inconnections){
-            Neuron leftNeuron = con.getFromNeuron();
+            Neurons leftNeuron = con.getFromNeuron();
             double weight = con.getWeight();
             double a = leftNeuron.getOutput(); //output from previous layer
              
@@ -40,7 +40,7 @@ public class Neuron {
     }
      
     public void addInConnectionsS(ArrayList<Neuron> inNeurons){
-        for(Neuron n: inNeurons){
+        for(Neurons n: inNeurons){
             Connection con = new Connection(n,this);
             Inconnections.add(con);
             connectionLookup.put(n.id, con);
